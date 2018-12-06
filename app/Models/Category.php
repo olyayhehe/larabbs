60771b48a45da0d85738e7714bd7365bb2d75448
何兆勇 <407020997@qq.com>
2018-12-06 06:58:04 +0000

    Seeding topics and users data

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,12 +9,4 @@ class Category extends Model
     protected $fillable = [
         'name', 'description',
     ];
-
-    Schema::create('categories', function (Blueprint $table) {
-    $table->increments('id');
-    $table->string('name')->index()->comment('名称');
-    $table->text('description')->nullable()->comment('描述');
-    $table->integer('post_count')->default(0)->comment('帖子数');
-    $table->timestamps();
-    });
 }
