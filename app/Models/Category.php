e23eab5ca2574ba3786e5f226abdef1fba2efd51
何兆勇 <407020997@qq.com>
2018-12-06 03:48:06 +0000

    Create categories

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name', 'description',
+    ];
+
+    Schema::create('categories', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('name')->index()->comment('名称');
+    $table->text('description')->nullable()->comment('描述');
+    $table->integer('post_count')->default(0)->comment('帖子数');
+    $table->timestamps();
+    });
+}
