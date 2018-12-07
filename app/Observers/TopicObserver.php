a5283dd03736ddf14165e39d6b973e01887c47b0
何兆勇 <407020997@qq.com>
2018-12-07 12:21:44 +0000

    User can create topic

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -13,9 +13,25 @@ class TopicObserver
     {
         //
     }
-
     public function updating(Topic $topic)
     {
         //
     }
+    public function saving(Topic $topic)
+    {
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
