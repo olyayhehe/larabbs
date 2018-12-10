1f8fdfbc234756798873bebee7f9998ce2fd6f9d
何兆勇 <407020997@qq.com>
2018-12-10 07:37:59 +0000

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,12 @@ class User extends Authenticatable
         return $this->hasMany(Topic::class);
     }
 
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+
 }
 
 
