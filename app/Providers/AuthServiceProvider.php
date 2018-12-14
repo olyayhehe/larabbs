6a967bbbe207bba450c90b77ecee94d0597aa582
何兆勇 <407020997@qq.com>
2018-12-14 02:55:58 +0000

    Add manage contents permission and visit horizon dashboard permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,19 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
